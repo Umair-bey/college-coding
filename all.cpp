76019301b4a72316cf552348a9317221620f6f58
Umair-bey <umair2025khan.in@gmail.com>
2025-06-03 23:10:16 +0300

    Add files via upload

---
+++ all.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() { //variables aare like containers ,eg:int , string, bool
+  cout<<"lets practice with variables\n";
+  cout<<"operations \n ";
+  cout<< " 5+2 is "<<5+2<<endl;
+  cout<< " 15-2 is "<<15-2<<endl;
+  cout<< " 15*2 is "<<15*2<<endl;
+  cout<< " 15^2 is "<<15*15<<endl;
+  cout<< " 14/2 is "<<14/2<<endl;
+  
+}
