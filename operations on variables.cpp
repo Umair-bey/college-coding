e4cbeade62672348aff6bf1458e5d80db7ddc423
Umair-bey <umair2025khan.in@gmail.com>
2025-06-04 13:06:24 +0300

    Add files via upload

---
+++ operations on variables.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;    
+int main() {
+   int x;
+    x=15;
+   int y;
+   y=10;
+   cout<<"x+y is  " <<x+y<<endl;
+   cout<<"x*y is  " <<x*y<<endl;
+   cout<<"2x+y is  " <<x*2+y<<endl;
+   cout<<"2x/y is  " <<x*2/y<<endl;
+   cout<<"(x)^2/y is  " <<x*x/y<<endl;
+   cout<<"2(x)^2/5y is  " <<2*x*x/5/y;
+    }
+
