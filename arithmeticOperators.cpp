9496ee1fc08b01d15e728258bb598359bfd2e695
Umair-bey <umair2025khan.in@gmail.com>
2025-06-09 12:39:29 +0300

    Add files via upload

---
+++ arithmeticOperators.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;    
+int main() {
+   int x = 12,y = 5;
+cout<<"lets use int\n";
+cout<< "x+y is "<< x + y <<endl;
+cout<< "x-y is "<< x - y <<endl;
+cout<< "x*y is "<< x * y <<endl;
+cout<< "x/y is "<< x / y <<endl;//12 by 5 is 2.4 but it will give 2 as it is int
+cout<<"lets use floot\n";
+float a = 12.8,b = 5;
+cout<< "a + b is "<< a + b <<endl;
+cout<< "a - b is "<< a - b <<endl;
+cout<< "a x b is "<< a * b <<endl;
+cout<< "a / b is "<< a / b <<endl;
+}
